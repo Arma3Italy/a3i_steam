d0902bec912bf4b3692b8187f093076a769bdc0f
Kaj Oskar Rusilowski <xedom08@gmail.com>
2019-07-23 22:10:20 +0200

    added comments

--- userUpdaterBot.py
+++ userUpdaterBot.py
@@ -3,11 +3,13 @@ import requests as fetch
 import time
 import math
 
+# global variables
 api_key = '8E5D5A4C69D001BA20C1B71834DD0C56'
 api_gameinfo = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key='+api_key+'&format=json&steamid='
 api_user = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='+api_key+'&format=json&steamids='
 mongo_uri = 'mongodb+srv://admin:admin@arma3italy-eufgo.mongodb.net'
 
+# returns users data from steam, that are registered on mongodb
 def getUsersData(users):
 	users_id = []
 
@@ -23,12 +25,14 @@ def getUsersData(users):
 
 	return res
 
-def filterJson(jsonObj,nameP,valueP,valueR):
+# search object(jsonObj) in an array by some name(keyP) and value(valueP) defined and returns the desire key(keyR) value
+def filterJson(jsonObj,keyP,valueP,keyR):
 	for id in jsonObj:
-		if id[nameP] == valueP:
-			return id[valueR]
+		if id[keyP] == valueP:
+			return id[keyR]
 	return 0
 
+# returns the hour played on arma
 def armaH(x):
 	try:
 		resAH = fetch.get(api_gameinfo+x)
@@ -41,6 +45,7 @@ def armaH(x):
 	except :
 		return 0
 
+# returns only needed data from steam api
 def filterUsers(x):
 	return {
 		'steamid': x['steamid'],
@@ -53,13 +58,22 @@ def filterUsers(x):
 	}
 
 def main():
+	# connection to the database
 	client = MongoClient(mongo_uri)
+
+	# select of the desired collection
 	coll_users = client.test.users
+
+	# getting all users of the db
 	users = list(coll_users.find())
 
+	# updating users data
 	usersData = getUsersData(users)
+
+	# parsing returned users data from steam
 	newUsers = list(map(filterUsers,usersData))
 
+	# sending updated data to db
 	for usr in newUsers:
 		userfilter = { 'steamid': usr['steamid'] }
 		userUpdate = {
